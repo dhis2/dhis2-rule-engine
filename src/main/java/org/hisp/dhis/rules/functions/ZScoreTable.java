eb950a9bab8bd52642444c6a95b42072a0af1fca
zubaira <zubair@dhis2.org>
2019-08-12 12:17:51 +0200

    added table from 0 to 5 years

--- src/main/java/org/hisp/dhis/rules/functions/ZScoreTable.java
+++ src/main/java/org/hisp/dhis/rules/functions/ZScoreTable.java
@@ -202,8 +202,42 @@ public class ZScoreTable
         zscoreMap.put( new ZScoreTableKey( (byte)1, (float)22 ), createSDMap( 75.2f,78.4f,81.5f,84.6f,87.7f,90.8f,94.0f ) );
         zscoreMap.put( new ZScoreTableKey( (byte)1, (float)23 ), createSDMap( 76.0f,79.2f,82.3f,85.5f,88.7f,91.9f,95.0f ) );
         zscoreMap.put( new ZScoreTableKey( (byte)1, (float)24 ), createSDMap( 76.7f,80.0f,83.2f,86.4f,89.6f,92.9f,96.1f ) );
-
-
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)25 ), createSDMap( 76.8f,80.0f,83.3f,86.6f,89.9f,93.1f,96.4f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)26 ), createSDMap( 77.5f,80.8f,84.1f,87.4f,90.8f,94.1f,97.4f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)27 ), createSDMap( 78.1f,81.5f,84.9f,88.3f,91.7f,95.0f,98.4f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)28 ), createSDMap( 78.8f,82.2f,85.7f,89.1f,92.5f,96.0f,99.4f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)29 ), createSDMap( 79.5f,82.9f,86.4f,89.9f,93.4f,96.9f,100.3f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)30 ), createSDMap( 80.1f,83.6f,87.1f,90.7f,94.2f,97.7f,101.3f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)31 ), createSDMap( 80.7f,84.3f,87.9f,91.4f,95.0f,98.6f,102.2f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)32 ), createSDMap( 81.3f,84.9f,88.6f,92.2f,95.8f,99.4f,103.1f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)33 ), createSDMap( 81.9f,85.6f,89.3f,92.9f,96.6f,100.3f,103.9f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)34 ), createSDMap( 82.5f,86.2f,89.9f,93.6f,97.4f,101.1f,104.8f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)35 ), createSDMap( 83.1f,86.8f,90.6f,94.4f,98.1f,101.9f,105.6f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)36 ), createSDMap( 83.6f,87.4f,91.2f,95.1f,98.9f,102.7f,106.5f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)37 ), createSDMap( 84.2f,88.0f,91.9f,95.7f,99.6f,103.4f,107.3f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)38 ), createSDMap( 84.7f,88.6f,92.5f,96.4f,100.3f,104.2f,108.1f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)39 ), createSDMap( 85.3f,89.2f,93.1f,97.1f,101.0f,105.0f,108.9f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)40 ), createSDMap( 85.8f,89.8f,93.8f,97.7f,101.7f,105.7f,109.7f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)41 ), createSDMap( 86.3f,90.4f,94.4f,98.4f,102.4f,106.4f,110.5f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)42 ), createSDMap( 86.8f,90.9f,95.0f,99.0f,103.1f,107.2f,111.2f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)43 ), createSDMap( 87.4f,91.5f,95.6f,99.7f,103.8f,107.9f,112.0f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)44 ), createSDMap( 87.9f,92.0f,96.2f,100.3f,104.5f,108.6f,112.7f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)45 ), createSDMap( 88.4f,92.5f,96.7f,100.9f,105.1f,109.3f,113.5f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)46 ), createSDMap( 88.9f,93.1f,97.3f,101.5f,105.8f,110.0f,114.2f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)47 ), createSDMap( 89.3f,93.6f,97.9f,102.1f,106.4f,110.7f,114.9f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)48 ), createSDMap( 89.8f,94.1f,98.4f,102.7f,107.0f,111.3f,115.7f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)49 ), createSDMap( 90.3f,94.6f,99.0f,103.3f,107.7f,112.0f,116.4f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)50 ), createSDMap( 90.7f,95.1f,99.5f,103.9f,108.3f,112.7f,117.1f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)51 ), createSDMap( 91.2f,95.6f,100.1f,104.5f,108.9f,113.3f,117.7f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)52 ), createSDMap( 91.7f,96.1f,100.6f,105.0f,109.5f,114.0f,118.4f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)53 ), createSDMap( 92.1f,96.6f,101.1f,105.6f,110.1f,114.6f,119.1f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)54 ), createSDMap( 92.6f,97.1f,101.6f,106.2f,110.7f,115.2f,119.8f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)55 ), createSDMap( 93.0f,97.6f,102.2f,106.7f,111.3f,115.9f,120.4f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)56 ), createSDMap( 93.4f,98.1f,102.7f,107.3f,111.9f,116.5f,121.1f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)57 ), createSDMap( 93.9f,98.5f,103.2f,107.8f,112.5f,117.1f,121.8f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)58 ), createSDMap( 94.3f,99.0f,103.7f,108.4f,113.0f,117.7f,122.4f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)59 ), createSDMap( 94.7f,99.5f,104.2f,108.9f,113.6f,118.3f,123.1f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)60 ), createSDMap( 95.2f,99.9f,104.7f,109.4f,114.2f,118.9f,123.7f ) );
 
         return zscoreMap;
     }
@@ -236,7 +270,43 @@ public class ZScoreTable
         zscoreMap.put( new ZScoreTableKey( (byte)0, (float)21 ), createSDMap( 76.5f,79.4f,82.3f,85.1f,88.0f,90.9f,93.8f ) );
         zscoreMap.put( new ZScoreTableKey( (byte)0, (float)22 ), createSDMap( 77.2f,80.2f,83.1f,86.0f,89.0f,91.9f,94.9f ) );
         zscoreMap.put( new ZScoreTableKey( (byte)0, (float)23 ), createSDMap( 78.0f,81.0f,83.9f,86.9f,89.9f,92.9f,95.9f ) );
-        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)24 ), createSDMap( 78.7f,81.7f,84.8f,87.8f,90.9f,93.9f,97.0f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)24 ), createSDMap( 78.0f,81.0f,84.1f,87.1f,90.2f,93.2f,96.3f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)25 ), createSDMap( 78.6f,81.7f,84.9f,88.0f,91.1f,94.2f,97.3f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)26 ), createSDMap( 79.3f,82.5f,85.6f,88.8f,92.0f,95.2f,98.3f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)27 ), createSDMap( 79.9f,83.1f,86.4f,89.6f,92.9f,96.1f,99.3f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)28 ), createSDMap( 80.5f,83.8f,87.1f,90.4f,93.7f,97.0f,100.3f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)29 ), createSDMap( 81.1f,84.5f,87.8f,91.2f,94.5f,97.9f,101.2f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)30 ), createSDMap( 81.7f,85.1f,88.5f,91.9f,95.3f,98.7f,102.1f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)31 ), createSDMap( 82.3f,85.7f,89.2f,92.7f,96.1f,99.6f,103.0f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)32 ), createSDMap( 82.8f,86.4f,89.9f,93.4f,96.9f,100.4f,103.9f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)33 ), createSDMap( 83.4f,86.9f,90.5f,94.1f,97.6f,101.2f,104.8f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)34 ), createSDMap( 83.9f,87.5f,91.1f,94.8f,98.4f,102.0f,105.6f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)35 ), createSDMap( 84.4f,88.1f,91.8f,95.4f,99.1f,102.7f,106.4f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)36 ), createSDMap( 85.0f,88.7f,92.4f,96.1f,99.8f,103.5f,107.2f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)37 ), createSDMap( 85.5f,89.2f,93.0f,96.7f,100.5f,104.2f,108.0f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)38 ), createSDMap( 86.0f,89.8f,93.6f,97.4f,101.2f,105.0f,108.8f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)39 ), createSDMap( 86.5f,90.3f,94.2f,98.0f,101.8f,105.7f,109.5f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)40 ), createSDMap( 87.0f,90.9f,94.7f,98.6f,102.5f,106.4f,110.3f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)41 ), createSDMap( 87.5f,91.4f,95.3f,99.2f,103.2f,107.1f,111.0f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)42 ), createSDMap( 88.0f,91.9f,95.9f,99.9f,103.8f,107.8f,111.7f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)43 ), createSDMap( 88.4f,92.4f,96.4f,100.4f,104.5f,108.5f,112.5f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)44 ), createSDMap( 88.9f,93.0f,97.0f,101.0f,105.1f,109.1f,113.2f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)45 ), createSDMap( 89.4f,93.5f,97.5f,101.6f,105.7f,109.8f,113.9f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)46 ), createSDMap( 89.8f,94.0f,98.1f,102.2f,106.3f,110.4f,114.6f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)47 ), createSDMap( 90.3f,94.4f,98.6f,102.8f,106.9f,111.1f,115.2f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)48 ), createSDMap( 90.7f,94.9f,99.1f,103.3f,107.5f,111.7f,115.9f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)49 ), createSDMap( 91.2f,95.4f,99.7f,103.9f,108.1f,112.4f,116.6f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)50 ), createSDMap( 91.6f,95.9f,100.2f,104.4f,108.7f,113.0f,117.3f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)51 ), createSDMap( 92.1f,96.4f,100.7f,105.0f,109.3f,113.6f,117.9f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)52 ), createSDMap( 92.5f,96.9f,101.2f,105.6f,109.9f,114.2f,118.6f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)53 ), createSDMap( 93.0f,97.4f,101.7f,106.1f,110.5f,114.9f,119.2f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)54 ), createSDMap( 93.4f,97.8f,102.3f,106.7f,111.1f,115.5f,119.9f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)55 ), createSDMap( 93.9f,98.3f,102.8f,107.2f,111.7f,116.1f,120.6f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)56 ), createSDMap( 94.3f,98.8f,103.3f,107.8f,112.3f,116.7f,121.2f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)57 ), createSDMap( 94.7f,99.3f,103.8f,108.3f,112.8f,117.4f,121.9f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)58 ), createSDMap( 95.2f,99.7f,104.3f,108.9f,113.4f,118.0f,122.6f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)59 ), createSDMap( 95.6f,100.2f,104.8f,109.4f,114.0f,118.6f,123.2f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)60 ), createSDMap( 96.1f,100.7f,105.3f,110.0f,114.6f,119.2f,123.9f ) );
 
         return zscoreMap;
     }
@@ -376,6 +446,26 @@ public class ZScoreTable
         zscoreMap.put( new ZScoreTableKey( (byte)1, (float)109 ), createSDMap( 13.7f,15.0f,16.4f,18.0f,19.7f,21.8f,24.2f ) );
         zscoreMap.put( new ZScoreTableKey( (byte)1, (float)109.5 ), createSDMap( 13.9f,15.1f,16.5f,18.1f,20.0f,22.0f,24.4f ) );
         zscoreMap.put( new ZScoreTableKey( (byte)1, (float)110 ), createSDMap( 14.0f,15.3f,16.7f,18.3f,20.2f,22.3f,24.7f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)110.5 ), createSDMap( 14.4f,15.7f,17.1f,18.8f,20.7f,22.9f,25.4f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)111 ), createSDMap( 14.5f,15.8f,17.3f,19.0f,20.9f,23.1f,25.7f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)111.5 ), createSDMap( 14.7f,16.0f,17.5f,19.2f,21.2f,23.4f,26.0f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)112 ), createSDMap( 0.09507f,14.8f,16.2f,17.7f,19.4f,21.4f,23.6f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)112.5 ), createSDMap( 15.0f,16.3f,17.9f,19.6f,21.6f,23.9f,26.5f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)113 ), createSDMap( 15.1f,16.5f,18.0f,19.8f,21.8f,24.2f,26.8f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)113.5 ), createSDMap( 15.3f,16.7f,18.2f,20.0f,22.1f,24.4f,27.1f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)114 ), createSDMap( 15.4f,16.8f,18.4f,20.2f,22.3f,24.7f,27.4f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)114.5 ), createSDMap( 15.6f,17.0f,18.6f,20.5f,22.6f,25.0f,27.8f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)115 ), createSDMap( 15.7f,17.2f,18.8f,20.7f,22.8f,25.2f,28.1f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)115.5 ), createSDMap( 15.9f,17.3f,19.0f,20.9f,23.0f,25.5f,28.4f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)116 ), createSDMap( 16.0f,17.5f,19.2f,21.1f,23.3f,25.8f,28.7f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)116.5 ), createSDMap( 16.2f,17.7f,19.4f,21.3f,23.5f,26.1f,29.0f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)117 ), createSDMap( 16.3f,17.8f,19.6f,21.5f,23.8f,26.3f,29.3f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)117.5 ), createSDMap( 16.5f,18.0f,19.8f,21.7f,24.0f,26.6f,29.6f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)118 ), createSDMap( 16.6f,18.2f,19.9f,22.0f,24.2f,26.9f,29.9f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)118.5 ), createSDMap( 16.8f,18.4f,20.1f,22.2f,24.5f,27.2f,30.3f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)119 ), createSDMap( 16.9f,18.5f,20.3f,22.4f,24.7f,27.4f,30.6f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)119.5 ), createSDMap( 17.1f,18.7f,20.5f,22.6f,25.0f,27.7f,30.9f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)1, (float)120 ), createSDMap( 17.3f,18.9f,20.7f,22.8f,25.2f,28.0f,31.2f ) );
 
         return zscoreMap;
     }
@@ -515,6 +605,26 @@ public class ZScoreTable
         zscoreMap.put( new ZScoreTableKey( (byte)0, (float)109 ), createSDMap( 14f,15.1f,16.5f,17.9f,19.6f,21.4f,23.6f ) );
         zscoreMap.put( new ZScoreTableKey( (byte)0, (float)109.5 ), createSDMap( 14.1f,15.3f,16.6f,18.1f,19.8f,21.7f,23.8f ) );
         zscoreMap.put( new ZScoreTableKey( (byte)0, (float)110 ), createSDMap( 14.2f,15.4f,16.8f,18.3f,20f,21.9f,24.1f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)110.5 ), createSDMap( 14.5f,15.8f,17.1f,18.7f,20.4f,22.4f,24.7f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)111 ), createSDMap( 14.6f,15.9f,17.3f,18.9f,20.7f,22.7f,25.0f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)111.5 ), createSDMap( 14.8f,16.0f,17.5f,19.1f,20.9f,22.9f,25.2f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)112 ), createSDMap( 14.9f,16.2f,17.6f,19.2f,21.1f,23.1f,25.5f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)112.5 ), createSDMap( 15.0f,16.3f,17.8f,19.4f,21.3f,23.4f,25.8f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)113 ), createSDMap( 15.2f,16.5f,18.0f,19.6f,21.5f,23.6f,26.0f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)113.5 ), createSDMap( 15.3f,16.6f,18.1f,19.8f,21.7f,23.9f,26.3f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)114 ), createSDMap( 15.4f,16.8f,18.3f,20.0f,21.9f,24.1f,26.6f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)114.5 ), createSDMap( 15.6f,16.9f,18.5f,20.2f,22.1f,24.4f,26.9f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)115 ), createSDMap( 15.7f,17.1f,18.6f,20.4f,22.4f,24.6f,27.2f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)115.5 ), createSDMap( 15.8f,17.2f,18.8f,20.6f,22.6f,24.9f,27.5f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)116 ), createSDMap( 16.0f,17.4f,19.0f,20.8f,22.8f,25.1f,27.8f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)116.5 ), createSDMap( 16.1f,17.5f,19.2f,21.0f,23.0f,25.4f,28.0f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)117 ), createSDMap( 16.2f,17.7f,19.3f,21.2f,23.3f,25.6f,28.3f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)117.5 ), createSDMap( 16.4f,17.9f,19.5f,21.4f,23.5f,25.9f,28.6f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)118 ), createSDMap( 16.5f,18.0f,19.7f,21.6f,23.7f,26.1f,28.9f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)118.5 ), createSDMap( 16.7f,18.2f,19.9f,21.8f,23.9f,26.4f,29.2f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)119 ), createSDMap( 16.8f,18.3f,20.0f,22.0f,24.1f,26.6f,29.5f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)119.5 ), createSDMap( 16.9f,18.5f,20.2f,22.2f,24.4f,26.9f,29.8f ) );
+        zscoreMap.put( new ZScoreTableKey( (byte)0, (float)120 ), createSDMap( 17.1f,18.6f,20.4f,22.4f,24.6f,27.2f,30.1f ) );
 
         return zscoreMap;
     }
