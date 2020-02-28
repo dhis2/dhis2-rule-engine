6b68d589a4e4c121ebd0ce5c853d90cdb419cd1f
Pablo <ppajueloc@gmail.com>
2020-02-28 07:59:23 +0100

    fix: Fixed decimal format for zScore calculations
    Decimal fixes

--- src/main/java/org/hisp/dhis/rules/functions/RuleFunctionZScore.java
+++ src/main/java/org/hisp/dhis/rules/functions/RuleFunctionZScore.java
@@ -56,7 +56,7 @@ public abstract class RuleFunctionZScore
     {
         DecimalFormatSymbols decimalFormatSymbols = DecimalFormatSymbols.getInstance();
         decimalFormatSymbols.setDecimalSeparator( '.' );
-        return new DecimalFormat( "###.00", decimalFormatSymbols );
+        return new DecimalFormat( "##0.00", decimalFormatSymbols );
     }
 
     @Override
