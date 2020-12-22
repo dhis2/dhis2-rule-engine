a0e406d8970db32af2bfb58ace66352ce0860622
Enrico Colasante <enrico.colasante@gmail.com>
2020-12-22 16:06:36 +0100

    Adding JavaDoc

--- src/main/java/org/hisp/dhis/rules/models/AttributeType.java
+++ src/main/java/org/hisp/dhis/rules/models/AttributeType.java
@@ -1,5 +1,8 @@
 package org.hisp.dhis.rules.models;
 
+/**
+ * This Enum specify the type of the id saved in the field of a RuleAction.
+ */
 public enum AttributeType {
     DATA_ELEMENT,
     TRACKED_ENTITY_ATTRIBUTE,
