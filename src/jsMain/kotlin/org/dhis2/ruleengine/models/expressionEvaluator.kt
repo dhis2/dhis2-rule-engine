062049127cab74e216dc3f5759ac0f3a85e374f0
Pablo <ppajueloc@gmail.com>
2023-01-19 11:14:19 +0100

    add kotlin expression evaluator to JS

--- src/jsMain/kotlin/org/dhis2/ruleengine/models/expressionEvaluator.kt
+++ src/jsMain/kotlin/org/dhis2/ruleengine/models/expressionEvaluator.kt
@@ -1,16 +1,28 @@
 package org.dhis2.ruleengine.models
 
+import org.dhis2.ruleengine.DataItem
 import org.dhis2.ruleengine.RuleVariableValue
+import org.dhis2.ruleengine.exprk.Expressions
 
 actual fun expressionEvaluator(): ExpressionParserEvaluator {
-   return object: ExpressionParserEvaluator{
-       override fun evaluate(
-           condition: String,
-           valueMap: Map<String, RuleVariableValue>,
-           supplementaryData: Map<String, List<String>>
-       ): String {
-           return ""
-       }
+    return object : ExpressionParserEvaluator {
+        override fun evaluate(
+            condition: String,
+            valueMap: Map<String, RuleVariableValue>,
+            supplementaryData: Map<String, List<String>>
+        ): String {
+            return Expressions()
+                .withValueMap(valueMap)
+                .eval(condition)
+        }
 
-   }
+        override fun getExpressionDescription(
+            expression: String,
+            dataItemStore: Map<String, DataItem>,
+            castAsBoolean: Boolean
+        ): String {
+            TODO("Not yet implemented")
+        }
+
+    }
 }
