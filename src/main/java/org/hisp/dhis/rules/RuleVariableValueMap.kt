a712dd9781e652422753ce7c2d7741d6f575fedb
Enrico <enrico@dhis2.org>
2023-12-06 12:08:13 +0100

    refactor ruleVariableValueMap to kotlin

---
+++ src/main/java/org/hisp/dhis/rules/RuleVariableValueMap.kt
@@ -0,0 +1,17 @@
+package org.hisp.dhis.rules
+
+import org.hisp.dhis.rules.models.RuleEnrollment
+import org.hisp.dhis.rules.models.RuleEvent
+import javax.annotation.Nonnull
+
+data class RuleVariableValueMap(
+    val enrollmentMap: Map<RuleEnrollment, Map<String, RuleVariableValue>>,
+    val eventMap: Map<RuleEvent, Map<String, RuleVariableValue>>
+) {
+    fun enrollmentMap(): Map<RuleEnrollment, Map<String, RuleVariableValue>> {
+        return enrollmentMap
+    }
+    fun eventMap(): Map<RuleEvent, Map<String, RuleVariableValue>> {
+        return eventMap
+    }
+}
