0a324eb285a1bcdcb099dd8384b86e8137fb80c8
Enrico <enrico@dhis2.org>
2024-12-13 11:32:46 -0300

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
     maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
 }
 
-version = "3.2.0-SNAPSHOT"
+version = "3.3.0-SNAPSHOT"
 group = "org.hisp.dhis.rules"
 
 if (project.hasProperty("removeSnapshotSuffix")) {
