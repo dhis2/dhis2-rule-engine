7a9cdf716cac7ce5b7de9a29d2e647b449275eaa
Enrico <enrico@dhis2.org>
2025-12-01 10:42:56 -0300

    fix: Always create supplementary data value keys [DHIS2-19426]

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
     maven { url = uri("https://central.sonatype.com/repository/maven-snapshots/") }
 }
 
-version = "3.4.0-SNAPSHOT"
+version = "3.4.1-SNAPSHOT"
 group = "org.hisp.dhis.rules"
 
 if (project.hasProperty("removeSnapshotSuffix")) {
