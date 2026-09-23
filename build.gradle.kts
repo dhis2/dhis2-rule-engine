17997a7b34887d5218e8f22d750e6a3e532211e8
Enrico Colasante <enrico@dhis2.org>
2026-09-23 11:31:45 +0200

    chore: Bump expression parser version to 1.4.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
     maven { url = uri("https://central.sonatype.com/repository/maven-snapshots/") }
 }
 
-version = "3.8.2-SNAPSHOT"
+version = "3.8.3-SNAPSHOT"
 group = "org.hisp.dhis.rules"
 
 if (project.hasProperty("removeSnapshotSuffix")) {
